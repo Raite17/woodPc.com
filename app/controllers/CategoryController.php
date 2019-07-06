e3bd67e41caa141ec312088baf45a8f4b1a746a2
Raite17 <survivarian@gmail.com>
2019-07-07 02:40:02 +0600

    Class Pagination was created.

--- app/controllers/CategoryController.php
+++ app/controllers/CategoryController.php
@@ -3,7 +3,10 @@
 
 namespace app\controllers;
 
+use app\models\Breadcrumbs;
 use app\models\Category;
+use woodpc\App;
+use woodpc\libs\Pagination;
 
 class CategoryController extends AppController
 {
@@ -14,13 +17,17 @@ class CategoryController extends AppController
         if (!$category) {
             throw new \Exception('Страница не найдена!', 404);
         }
-        //breadcrumbs
-        $breadcrumbs = '';
+        $breadcrumbs = Breadcrumbs::getBreadCrumbs($category->id);
         $categoryModel = new Category();
         $ids = $categoryModel->getIds($category->id);
         $ids = !$ids ? $category->id : $ids . $category->id;
-        $products = \R::find('products', "category_id IN ($ids)");
-        $this->setMeta($category->title,$category->description,$category->keywords);
-        $this->set(compact('products','breadcrumbs'));
+        $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+        $perPage = App::$app->getProperty('pagination');
+        $countTotal = \R::count('products', "category_id IN ($ids)");
+        $pagination = new Pagination($page, $perPage, $countTotal);
+        $start = $pagination->getStart();
+        $products = \R::find('products', "category_id IN ($ids) LIMIT $start,$perPage");
+        $this->setMeta($category->title, $category->description, $category->keywords);
+        $this->set(compact('products', 'breadcrumbs', 'pagination', 'countTotal'));
     }
 }
--- app/views/Category/view.php
+++ app/views/Category/view.php
@@ -3,8 +3,7 @@
     <div class="container">
         <div class="breadcrumbs-main">
             <ol class="breadcrumb">
-                <li><a href="<?=PATH;?>">Главная</a></li>
-                <li>Категория</li>
+                <?= $breadcrumbs ?>
             </ol>
         </div>
     </div>
@@ -15,24 +14,34 @@
     <div class="container">
         <div class="prdt-top">
             <div class="col-md-9 prdt-left">
-                <?php if(!empty($products)): ?>
+                <?php if (!empty($products)): ?>
                     <div class="product-one">
                         <?php $currency = \woodpc\App::$app->getProperty('currency'); ?>
-                        <?php foreach($products as $product): ?>
+                        <?php foreach ($products as $product): ?>
                             <div class="col-md-4 product-left p-left">
                                 <div class="product-main simpleCart_shelfItem">
-                                    <a href="product/<?=$product->alias;?>" class="mask"><img class="img-responsive zoom-img" src="images/<?=$product->img;?>" alt="" /></a>
+                                    <a href="product/<?= $product->alias; ?>" class="mask"><img
+                                                class="img-responsive zoom-img" src="images/<?= $product->img; ?>"
+                                                alt=""/></a>
                                     <div class="product-bottom">
-                                        <h3><?=$product->title;?></h3>
+                                        <h3><?= $product->title; ?></h3>
                                         <p>Explore Now</p>
                                         <h4>
-                                            <a data-id="<?=$product->id;?>" class="add-to-cart" href="cart/add?id=<?=$product->id;?>"><i></i></a> <span class=" item_price"><?=$currency['symbol_left'];?><?=$product->price * $currency['value'];?><?=$currency['symbol_right'];?></span>
+                                            <a data-id="<?= $product->id; ?>" class="add-to-cart"
+                                               href="cart/add?id=<?= $product->id; ?>"><i></i></a> <span
+                                                    class=" item_price"><?= $currency['symbol_left']; ?><?= $product->price * $currency['value']; ?><?= $currency['symbol_right']; ?></span>
                                         </h4>
                                     </div>
                                 </div>
                             </div>
                         <?php endforeach; ?>
                         <div class="clearfix"></div>
+                        <div class="text-center">
+                            <p>(<?=count($products)?> товара(ов) из <?=$countTotal;?>)</p>
+                            <?php if ($pagination->countPages > 1): ?>
+                                <?= $pagination ?>
+                            <?php endif; ?>
+                        </div>
                     </div>
                 <?php else: ?>
                     <h3>В этой категории товаров пока нет...</h3>
