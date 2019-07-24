14ae55718cecbcff61d30333d6d7c13abf2300b6
Raite17 <survivarian@gmail.com>
2019-07-24 23:09:26 +0600

    Admin LTE was included

---
+++ app/controllers/admin/AdminAppController.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace app\controllers\admin;
+use woodpc\base\Controller;
+
+class AdminAppController extends Controller
+{
+    public $layout = 'admin';
+}
---
+++ app/controllers/admin/MainController.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace app\controllers\admin;
+
+
+class MainController extends AdminAppController
+{
+    public function indexAction()
+    {
+
+    }
+}
